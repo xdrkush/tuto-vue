9a0130c33b7fea568cd34376ea50a39df872109e
xdrkush <drkushikush@gmail.com>
2020-06-04 20:05:29 +0200

    props init

---
+++ 09-props/src/components/titleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<!-- Title Components -->
+		<v-container class="fill-width" fluid>
+			<v-row align="center" justify="center">
+				<v-card-text>
+					<div class="display-4 font-weight-bold text-center">{{ titre }}</div>
+					<p class="display-1 text-center text--primary font-italic">{{ stitle }}</p>
+				</v-card-text>
+			</v-row>
+		</v-container>
+		<!-- / Title Components -->
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		subTitle: 'This Tuto is realize with',
+		framework: 'Vuetify'
+	}),
+	computed: {
+		stitle: function () {
+			return this.subTitle + ' ' + this.framework
+		}
+	},
+	props: {
+		titre: String
+	}
+};
+</script>
---
+++ 09-props/src/pages/HomePage.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+    
+		<!-- Title Components -->
+		<titleComponent titre='Welcome To VueJS !' />
+		<!-- / Title Components -->
+
+		<!-- Search Bar Component -->
+		<searchComponent v-on:searchToParent='getChildSearch' />
+		<!-- / Search Bar Components -->
+
+		<!-- Card News Components -->
+		<newsComponent titre='News :' :dataNews='newsData' />
+		<!-- / Card News Components -->
+
+		<!-- List Result Components -->
+		<resultComponent titre='Liste Result :' />
+		<!-- / List Result Components -->
+
+	</div>
+</template>
+
+<script>
+import titleComponent from "../components/titleComponent";
+import newsComponent from "../components/newsComponent";
+import resultComponent from "../components/resultComponent";
+import searchComponent from "../components/searchComponent";
+
+export default {
+	components: {
+		titleComponent,
+		searchComponent,
+		newsComponent,
+		resultComponent
+	},
+	data: () => ({
+		search: '',
+		newsData: [
+				{
+					title: 'Our Changing Planet',
+					author: 'Kurt Wagner',
+					image: 'https://cdn.vuetifyjs.com/images/cards/mountain.jpg',
+					message: 'Visit ten places on our planet that are undergoing the biggest changes today.'
+				},
+				{
+					title: 'Our Changing Planet',
+					author: 'Kurt Wagner',
+					image: 'https://cdn.vuetifyjs.com/images/cards/mountain.jpg',
+					message: 'Visit ten places on our planet that are undergoing the biggest changes today.'
+				},
+				{
+					title: 'Our Changing Planet',
+					author: 'Kurt Wagner',
+					image: 'https://cdn.vuetifyjs.com/images/cards/mountain.jpg',
+					message: 'Visit ten places on our planet that are undergoing the biggest changes today.'
+				}
+		]
+	}),
+	methods: {
+		getChildSearch: (value) => {
+			alert(value)
+    }
+	}
+};
+</script>
